b8a3a2a2f512f188a74027e94020e65d8c7bb71a
Raul Souto <raulvprsouto@gmail.com>
2024-10-30 14:55:40 -0300

    resolvi o problema de cores de fundo sumindo

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 let elementCount = 0; // Contador global para IDs de elementos
 
-function back() {
-    document.getElementById("box1").style.background = "none";
-    document.getElementById("box1").style.border = "none";
-}
-
 function allowDrop(event) {
     event.preventDefault();
     event.target.classList.add('dragover');
 }
 
 function drag(event) {
+    console.log('drag');
     event.dataTransfer.setData("a", event.target.id);
 }
 
